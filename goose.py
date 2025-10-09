d551c37daa9c347436fb16db745570d3d405dd99
stud31 <stud31@redi-school.org>
2025-10-09 18:59:22 +0200

    Update goose.py

--- goose.py
+++ goose.py
@@ -8,6 +8,25 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Tamagoose")
 
+game_on = True
+
+screen_color = (198,219,255)
+
+while game_on:
+    screen.fill(screen_color)
+    
+
+#spielcode
+
+for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_on = False
+        screen.fill(screen_color)
+        
+        pygame.display.upgrade()
+
+pygame.quit()
+
 
 
 #spielcode
@@ -16,3 +35,4 @@ pygame.display.set_caption("Tamagoose")
 
 
 pygame.quit()
+
