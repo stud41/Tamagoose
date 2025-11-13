11512acf101da0676e8aa03b9a5f06aae35bdf6c
Anna <stud35@redi-school.org>
2025-11-13 18:50:30 +0100

    Update goose.py

--- goose.py
+++ goose.py
@@ -16,12 +16,17 @@ TEXT_COLOR = (255, 255, 255)
 # 1=klein, 2=mittel, 3=groß
 goose_grosse = 1
 
-goose_1 = pygame.image.load("C:/Users/redi-user/Desktop/SMALL GOOSE PNG.png")
+goose_1 = pygame.image.load("C:/Users/ReDI User/Pictures/spiel_!!/SMALL GOOSE PNG.png")
 goose_1 = pygame.transform.scale(goose_1, (800, 600))
 # 1=normal, 2=baden, 3=schlafen, 4=essen
 goose_machen = 1
 
-bg = pygame.image.load("C:/Users/redi-user/Desktop/GANZES BACKROUND.png")
+goose_essen = 1
+goose_schlafen = 1
+goose_gluck = 1
+goose_baden = 1
+
+bg = pygame.image.load("C:/Users/ReDI User/Desktop/GANZES BACKROUND.png")
 bg = pygame.transform.scale(bg, (800, 600))
 
 button_pressed = False
@@ -33,13 +38,21 @@ Menu_color = 255, 246, 189
 font = pygame.font.SysFont(None, 36)
 
 button_rect = pygame.Rect(20, 20, 100, 50)# x, y, width, height
-food_rect = pygame.Rect(170, 240, 150, 60)
+
+food_rect = pygame.image.load("C:/Users/ReDI User/Downloads/FÜTTERN KNOPF.png")#pygame.Rect(200, 370, 150, 60)
+food_rect = pygame.transform.scale(food_rect, (266, 200))
+fake_food = pygame.Rect(160, 300, 266, 200)
+sleep_rect = pygame.Rect(440, 370, 150, 60)
+gluck_rect = pygame.Rect(440, 260, 150, 60)
+bad_rect = pygame.Rect(200, 260, 150, 60)
+
+menu_button = False
 
 # Main loop
 running = True
 while running:
     mouse_pos = pygame.mouse.get_pos()
-    mouse_pressed = pygame.mouse.get_pressed()
+    #mouse_pressed = pygame.mouse.get_pressed()
 
     if first_run:
         screen.blit(bg, (0,0))
@@ -51,22 +64,37 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
         # Detect click inside button
         if event.type == pygame.MOUSEBUTTONDOWN and button_rect.collidepoint(event.pos):
             if not button_pressed:
-                print("button pressed")
+                #print("button pressed")
                 button_pressed = True
                 pygame.draw.rect(screen, (Menu_color), (rect_x, rect_y, rect_width, rect_height), 0, 20)
                 pygame.draw.rect(screen, (139, 69, 19), (rect_x-5, rect_y-5, rect_width+10, rect_height+10), 10, 30)
-                pygame.draw.rect(screen, (238, 154, 73), food_rect)
+                menu_button = True
             else:
-                print("this should actually delete the rectangle")
+                #print("this should actually delete the rectangle")
                 screen.blit(bg, (0,0))
                 button_pressed = False
-                first_run = True
+                menu_button = False
                 
-
+        if menu_button == True:
+            screen.blit(food_rect, (160, 300))
+            if event.type == pygame.MOUSEBUTTONDOWN and fake_food.collidepoint(event.pos):
+                goose_essen += 1
+                    
+            pygame.draw.rect(screen, (238, 154, 73), sleep_rect)
+            if event.type == pygame.MOUSEBUTTONDOWN and sleep_rect.collidepoint(event.pos):
+                goose_schlafen += 1
+                
+            pygame.draw.rect(screen, (238, 154, 73), gluck_rect)
+            if event.type == pygame.MOUSEBUTTONDOWN and gluck_rect.collidepoint(event.pos):
+                goose_gluck += 1
+                
+            pygame.draw.rect(screen, (238, 154, 73), bad_rect)
+            if event.type == pygame.MOUSEBUTTONDOWN and bad_rect.collidepoint(event.pos):
+                goose_baden += 1
+    
     # Change color on hover
     if button_rect.collidepoint(mouse_pos):
         color = BUTTON_HOVER
